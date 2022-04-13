ef20719056741b43d32c4ae6ac5d86bcd7b24ebb
wtzhu <1240355745@qq.com>
2022-04-13 09:52:18 +0800

    add HelloWord class

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * @ClassName: HelloWorld
+ * @Description: 经典
+ * @Author: zwt
+ * @Date: 2022/4/13 9:46
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+ }
